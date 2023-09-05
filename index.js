9b055e30807488e28885cd5158f65963ff84926d
Arun Kumar <aruncode49@gmail.com>
2023-09-05 13:53:47 +0530

    SSR-EJS , Creating UI FOR Url Shortener

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ const express = require("express");
 
 const { connectToMongoDB } = require("./connection");
 const urlRouter = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const app = express();
 const PORT = 8001;
 
+// Connection with MongoDB
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url");
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+// app.set("views", path.resolve("./views")); => optional because render bydefault check in views folder
+
 // Using Middleware for JSON Body
 app.use(express.json());
 
-// Connection with MongoDB
-connectToMongoDB("mongodb://127.0.0.1:27017/short-url");
+// Using Middleware for Form Data
+app.use(express.urlencoded({ extended: false }));
+
+// Use the static route for home page
+app.use("/", staticRoute);
 
 // Using Router
 app.use("/url", urlRouter);
